c2239f0a69d414082688d02ace72018a15513318
Hisanori Kuwauchi <hkuwauchi@gmail.com>
2020-06-18 23:15:19 +0900

    404わけわからん

--- OIDC/Startup.cs
+++ OIDC/Startup.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.OpenApi.Models;
 
 namespace OIDC
@@ -21,10 +26,59 @@ namespace OIDC
         {
             services.AddControllers();
 
+            string url = $"http://share-ubuntu:8080/auth/realms/{Configuration["Keycloak:Realm"]}/protocol/openid-connect/auth";
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OIDC API", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri(url, UriKind.Absolute),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                //{ "readAccess", "Access read operations" },
+                                //{ "writeAccess", "Access write operations" }
+                            }
+                        }
+                    }
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+                        },
+                        //new[] { "readAccess", "writeAccess" }
+                        Array.Empty<string>()
+                    }
+                });
+
+                c.OperationFilter<OAuth2OperationFilter>();
             });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(o => o.LoginPath = new PathString("/login"))
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                    options.Authority = $"http://share-ubuntu:8080/auth/realms/{Configuration["Keycloak:Realm"]}/protocol/openid-connect/auth";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ClientId = Configuration["Keycloak:ClientId"];
+                    options.ClientSecret = Configuration["Keycloak:Secret"];
+                    options.ResponseType = $"{OpenIdConnectParameterNames.Code} {OpenIdConnectParameterNames.AccessToken}";
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,16 +89,23 @@ namespace OIDC
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OIDC API V1");
+                c.OAuthClientId(Configuration["Keycloak:ClientId"]);
+                c.OAuthClientSecret(Configuration["Keycloak:Secret"]);
+                c.OAuthRealm(Configuration["Keycloak:Realm"]);
+                c.OAuthAppName(Configuration["Keycloak:AppName"]);
+                c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
             });
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseSwagger();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
